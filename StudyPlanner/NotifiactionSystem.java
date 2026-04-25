d03af492876632109e7a30ce94281f8be68c006a
Apisara <apisara5039@gmail.com>
2026-04-25 13:08:20 +0700

    Remove unused observer notification classes

--- StudyPlanner/NotifiactionSystem.java
+++
@@ -1,7 +0,0 @@
-package StudyPlanner;
-
-class NotificationSystem implements Observer {
-    public void update(String message) {
-        System.out.println("🔔 " + message);
-    }
-}
--- StudyPlanner/Observer.java
+++
@@ -1,5 +0,0 @@
-package StudyPlanner;
-
-interface Observer {
-    void update(String message);
-}
